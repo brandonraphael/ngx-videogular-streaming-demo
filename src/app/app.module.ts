6937d80878f0232d83f800422e94edb65755d68a
Brandon Raphael <braphael@clickup.com>
2023-07-25 13:49:07 -0700

    add scrub bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VgCoreModule } from '@videogular/ngx-videogular/core';
+import { VgControlsModule } from '@videogular/ngx-videogular/controls';
+import { VgOverlayPlayModule } from '@videogular/ngx-videogular/overlay-play';
+import { VgStreamingModule } from '@videogular/ngx-videogular/streaming';
+import { VgBufferingModule } from "@videogular/ngx-videogular/buffering";
+import { VideoPlayerComponent } from './video-player/video-player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoPlayerComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgStreamingModule,
+    VgBufferingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
